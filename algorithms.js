44f5c4eb75da5b7df52e6eeeda2ae80a4f21eabf
Blade Boles <bladerboles@gmail.com>
2020-03-12 15:20:50 -0600

    Actually finished 2 now :)

--- algorithms.js
+++ algorithms.js
@@ -58,30 +58,33 @@ function merge(left, right, array) {
 };
 
 //Quicksort
-
+// [4,2,1,3,5] 
 function quickSort(array, start = 0, end = array.length) {
   debugger;
   if (start >= end) {
     return array;
   }
-  const middle = partition(array, start, end);
+  const middle = partition(array, start, end); // middle = 1
+  console.log(middle)
   array = quickSort(array, start, middle);
   array = quickSort(array, middle + 1, end);
+  
   return array;
 }
 
 function partition(array, start, end) {
   debugger;
-  const pivot = array[end - 1];
-  let j = start;
-  for (let i = start; i < end - 1; i++) {
+  const pivot = array[start]; // 1 ([1, 2])
+  let j = start + 1;
+  for (let i = start + 1; i < end; i++) {
     if (array[i] <= pivot) {
+      console.log("i: ", i, "j: ", j)
       swap(array, i, j);
       j++;
     }
   }
-  swap(array, end-1, j);
-  return j;
+  swap(array, start, j - 1); // [2, 1]
+  return j - 1;
 }
 
 let arrayToSort = "89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5".split(' ');
@@ -89,12 +92,12 @@ let arrayToSort2 = arrayToSort.slice();
 
 arrayToSort = arrayToSort.map(element => parseInt(element))
 
-console.log ('Unsorted array 1 (numbers):', arrayToSort)
-console.log ('Unsorted array 2 (still strings):', arrayToSort2)
+// console.log ('Unsorted array 1 (numbers):', arrayToSort)
+// console.log ('Unsorted array 2 (still strings):', arrayToSort2)
 
 arrayToSort2 = arrayToSort.map(element => parseInt(element))
 
 
-console.log(quickSort(arrayToSort))
+console.log('Quick sorted array output:', quickSort([14, 17, 13, 15, 19, 10, 3, 16, 9, 12]))
 
-console.log(mergeSort(arrayToSort2))
+// console.log(mergeSort(arrayToSort2))
