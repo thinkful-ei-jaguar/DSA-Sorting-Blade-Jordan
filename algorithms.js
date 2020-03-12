d05c95732dd5c732330067e1460a8147f6605bcb
Blade Boles <bladerboles@gmail.com>
2020-03-12 13:48:03 -0600

    Finished #3

--- algorithms.js
+++ algorithms.js
@@ -72,7 +72,7 @@ function quickSort(array, start = 0, end = array.length) {
 
 function partition(array, start, end) {
   debugger;
-  const pivot = array[start];
+  const pivot = array[end - 1];
   let j = start;
   for (let i = start; i < end - 1; i++) {
     if (array[i] <= pivot) {
@@ -84,9 +84,7 @@ function partition(array, start, end) {
   return j;
 }
 
-quickSort([14, 17, 13, 15, 19, 10, 3, 16, 9, 12]);
+let arrayToSort = "89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5".split(' ');
+arrayToSort = arrayToSort.map(element => parseInt(element))
 
-
-          [3, 9, 10, 12, 19, 14, 17, 16, 13, 15] // 2a
-
-          [14, 13, 10, 3, 9, 12, 15, 16, 19, 17] // 2b
+console.log(quickSort(arrayToSort))
